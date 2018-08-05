a5fed66665e57e1250c37d9fd4c1177f0c1bfa6b
Robert <robertszekely93@gmail.com>
2018-08-05 22:10:36 +0300

    Shoot FireBall added

---
+++ Warlock/Assets/DestroyFireBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyFireBall : MonoBehaviour {
+
+    private Vector3 startPos;
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Start: "+ startPos);
+        Debug.Log("Current: " + transform.position);
+        Debug.Log("Distance: " + Vector3.Distance(startPos, transform.position));
+        if(Vector3.Distance(startPos,transform.position)>10f) {
+            Destroy(this.gameObject);
+        }
+	}
+}
--- Warlock/Assets/Standard Assets/CrossPlatformInput/Scripts/ButtonHandler.cs
+++ Warlock/Assets/Standard Assets/CrossPlatformInput/Scripts/ButtonHandler.cs
@@ -7,15 +7,24 @@ namespace UnityStandardAssets.CrossPlatformInput
     {
 
         public string Name;
+        public GameObject prefab;
+        public Transform player;
+        public Vector3 offset;
 
         void OnEnable()
         {
-
+            
         }
 
         public void SetDownState()
         {
             CrossPlatformInputManager.SetButtonDown(Name);
+            Debug.Log("aaaaaaaaaaaaaaaaaaaaaaa");
+            GameObject fireBall = Instantiate(prefab) as GameObject;
+            fireBall.transform.position = player.transform.position + offset;
+            Rigidbody rb = fireBall.GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(0, 0, 10);
+
         }
 
 
