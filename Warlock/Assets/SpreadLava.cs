7278c56aa6105b5a34186e85570338c79f24ffbc
Robert <robertszekely93@gmail.com>
2018-08-16 20:22:55 +0300

    Lava spread added

---
+++ Warlock/Assets/SpreadLava.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpreadLava : MonoBehaviour {
+
+    public float x = 0f;
+    public float y = 0f;
+    public float z = 0f;
+
+    public int maxInvokeNumber = 0;
+
+    private int invokeNumber = 0;
+
+	// Use this for initializa stion
+	void Start () {
+        InvokeRepeating("Spread", 10.0f, 10.0f);
+	}
+	
+    void Spread() {
+        if (invokeNumber++ < maxInvokeNumber)
+        {
+            Debug.Log("Spread");
+            transform.localScale = transform.localScale + new Vector3(x, y, z);
+        }
+        else {
+            Debug.Log("Spread cancelled");
+            CancelInvoke("Spread");
+        }
+    }
+}
